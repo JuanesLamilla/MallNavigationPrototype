e6aa6508aa1bf4a948a521f1afe3b70c65caec90
Juan Lamilla <juan.e.lamilla@gmail.com>
2023-05-26 19:10:00 -0400

    Modify captions

--- MallNav.py
+++ MallNav.py
@@ -8,17 +8,14 @@ import json
 
 from streamlit_folium import st_folium
 
+st.title('Mall Navigator Prototype :world_map: :shopping_bags:')
+
 store_list = ['Cineplex', 'Ardene', 'Safeway', "Urban Planet", "Dollarama"]
 STORE_LOCS = 'geodata/mall_data.geojson'
 reverse_antpath = False
 
 SPM_Location = (53.53096649685565, -113.2936327376925)
 map_SPM = folium.Map(location = SPM_Location, width = "100%", zoom_start = 17) # max zoom: 18
-
-st.title('Mall Navigator Prototype :world_map: :shopping_bags:')
-st.caption('Choose a start and end location to show the best path through the mall to reach your destination.')
-st.caption('This proof of concept demonstrate how to use geojson, folium, and a streamlit to show dynamic geographic information on an interactive dashboard. This page was created entirely in Python.')
-st.caption('Code can be found on [github](https://github.com/JuanesLamilla/MallNavigationPrototype). You can also check out my [portfolio](https://juaneslamilla.github.io/).')
 
 option_start = st.selectbox(
     'Where are you now?',
@@ -71,3 +68,9 @@ folium.plugins.AntPath(finalPath).add_to(map_SPM)
 
 # call to render Folium map in Streamlit
 st_data = st_folium(map_SPM, width=800, height=800)
+
+st.divider()
+st.caption('Choose a start and end location to show the best path through the Sherwood Park Mall to reach your destination.')
+st.caption('This proof-of-concept demonstrate how to use **:green[folium]** and **:green[streamlit]** to show dynamic geographic information (geojson) on an interactive dashboard. This page was created entirely in Python.')
+st.caption('Similar navigation dashboards can be easily created for hospitals, university campus\'s, and more.')
+st.caption('Code can be found on [github](https://github.com/JuanesLamilla/MallNavigationPrototype). You can also check out my [portfolio](https://juaneslamilla.github.io/).')
