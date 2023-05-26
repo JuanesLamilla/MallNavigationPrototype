dcc0c36fa2ff6e54539960ab36e21ad8a3479460
JuanesLamilla <juan.e.lamilla@gmail.com>
2023-05-26 15:07:10 -0400

    Add store loader

--- MallNav.py
+++ MallNav.py
@@ -8,10 +8,11 @@ import json
 
 from streamlit_folium import st_folium
 
-STORE_LIST = ['Cineplex', 'Ardene', 'Safeway', "Urban Planet", "Dollarama"]
+store_list = ['Cineplex', 'Ardene', 'Safeway', "Urban Planet", "Dollarama"]
+STORE_LOCS = 'geodata/mall_data.geojson'
 
-UMMlocation = (53.53096649685565, -113.2936327376925)
-map_UMM = folium.Map(location = UMMlocation, width = "100%", zoom_start = 17) # max zoom: 18
+SPM_Location = (53.53096649685565, -113.2936327376925)
+map_SPM = folium.Map(location = SPM_Location, width = "100%", zoom_start = 17) # max zoom: 18
 
 st.title('Mall Navigator Prototype :world_map: :shopping_bags:')
 st.caption('Choose a start and end location to show the best path through the mall to reach your destination.')
@@ -20,16 +21,18 @@ st.caption('Code can be found on [github](https://github.com/JuanesLamilla/MallN
 
 option_start = st.selectbox(
     'Where are you now?',
-    STORE_LIST)
+    store_list)
 
-end_location = list(STORE_LIST)
+end_location = list(store_list)
 end_location.remove(option_start)
 
 option_end = st.selectbox(
     'Where would you like to go?',
     end_location)
 
-
+folium.GeoJson(STORE_LOCS,
+               tooltip=folium.GeoJsonTooltip(fields=['name'])
+              ).add_to(map_SPM)
 
 # call to render Folium map in Streamlit
-st_data = st_folium(map_UMM, width=800, height=800)
+st_data = st_folium(map_SPM, width=800, height=800)
